71bcda6c2943c1add163074182ec5d11ecf4db6f
Jordan Wofford <jmwofford-codes@gmail.com>
2020-04-10 23:03:26 -0500

    established create route that creates user based on params from form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def index 
   end
+
+  def create 
+    user = User.new(user_params)
+  end
+  private
+    def user_params
+      params.require(:user).perrmit(:email,:password, :password_conformation)
+    end
 end
