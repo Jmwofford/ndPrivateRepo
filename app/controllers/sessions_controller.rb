43814645954680b6bd4610095987d33b99b39a1e
Jordan Wofford <jmwofford-codes@gmail.com>
2020-04-10 23:04:12 -0500

    established created session controller syntax that accepts login params to create user session based on userIf

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email:login_params[:email])
+    if user && user.authenticate(login_params[:password])
+      session[:user_id] = session.id
+      redirect_to '/dashboard'
+    else
+      flash[:login_errors] = ['invalid credentials']
+      redirect_to '/'
+    end
+  end
+
+  private
+    def login_params
+      params.require(:login).permit(:email,:password)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
 
   def create 
     user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/dashboard'
+    else
+      flash[:register_errors] = user.errors.full_messages
+      redirect_to '/'
   end
   private
     def user_params
